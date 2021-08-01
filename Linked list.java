c205ac19fedf8a27d13cde453db4866fce09a429
Paramjeet singh <67743296+param0205@users.noreply.github.com>
2021-08-01 23:26:16 +0530

    Update Linked list.java

--- Linked list.java
+++ Linked list.java
@@ -546,4 +546,209 @@ class Solution{
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//Sorted insert for circular linked list
 
+
+//Code by:Saksham Raj Seth
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+		data=d;
+		next=null;
+	}
+}
+class LL{
+Node head;
+Node tail;
+    public void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            tail = head;
+        } else {
+            tail.next = node;
+            tail = tail.next;
+        }
+    }
+ public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0) {
+            int n = sc.nextInt();
+            LL llist = new LL();
+			llist.head=null;
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+			int value=sc.nextInt();
+			Node phead=head;
+			while(phead.next!=null){
+				phead=phead.next;
+			}
+			phead.next=head;
+            Solution ob=new Solution();
+			head = ob.sortedInsert(head,value);
+			Node current = head;
+			
+			if(head != null){
+			    do{
+			        System.out.print(current.data+" ");
+			        current = current.next;
+			    }while(current != head);
+			}
+			System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+		data=d;
+		next=null;
+	}
+}*/
+class Solution
+{
+	public static Node sortedInsert(Node head,int data)
+         {
+            //add code here.
+            Node curr=head;
+            if(curr.data>data)
+            {
+             while(curr.next!=head){
+             curr=curr.next;
+             }
+             Node n=new Node(data);
+             n.next=curr.next;
+             curr.next=n;
+             head=n;
+             return head;
+            }
+            Node prev=null;
+            while(true)
+            {    
+                if(curr.data>=data){
+                Node n=new Node(data);
+                
+                n.next=prev.next;
+                
+                prev.next=n;
+                    return head;
+                }
+                prev=curr;
+                curr=curr.next;
+            }
+          
+         }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Detect Loop in linked list
+
+//Initial template code for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG
+{
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            if( x.detectLoop(head) )
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+
+class Solution {
+    //Function to check if the linked list has a loop.
+    public static boolean detectLoop(Node head){
+        // Add code here
+        HashMap<Node,Integer> m=new HashMap<Node,Integer>();
+        Node curr=head;
+        while(curr!=null)
+        {
+            if(m.containsKey(curr.next))
+            return true;
+            else
+            m.put(curr.next,curr.data);
+            curr=curr.next;
+        }
+        return false;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
